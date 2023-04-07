391d6809765f22e9077d4e27a8253257b35f57df
Housam Hegazy <geohousam@gmail.com>
2023-04-07 23:14:06 +0000

    HH

--- src/Context/PostimageList.jsx
+++ src/Context/PostimageList.jsx
@@ -6,7 +6,7 @@ const initialData = {
   count:0,
   housam:4,
   hassan:5,
-  profileimage:null
+  profileUrl:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png'
 };
 const INCREAMENT = 'INCREAMENT'
 const DECREAMENT = 'DECREAMENT'
