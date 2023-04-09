31223ab5c284f98c6c5e81a80754c9db3cd39c47
housamhegazy <geohousam@gmail.com>
2023-04-09 20:42:37 +0300

    dddddddddddd

--- src/pages/Friends.jsx
+++ src/pages/Friends.jsx
@@ -18,3 +18,49 @@
 //     </div>
 //   )
 // }
+import React, { useState } from "react";
+const initialData = [
+  { id: 0, text: "Visit Kafka Museum", done: true },
+  { id: 1, text: "Watch a puppet show", done: false },
+  { id: 2, text: "Lennon Wall pic", done: false },
+];
+let nextId = 3;
+export default function Friends() {
+  const [data, setData] = useState(initialData);
+  const [inputData, setinputData] = useState();
+  function onChanges(e) {
+    setinputData({ text: e.target.value, done: true });
+  }
+  function addData() {
+    setData([...data, { ...inputData, id: nextId++ }]);
+  }
+  console.log(data);
+
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          onChanges(e);
+        }}
+        type="text"
+      />
+      <button
+        onClick={() => {
+          addData();
+        }}
+      >
+        add
+      </button>
+      {data.map((t) => {
+        return (
+          <div key={t.id}>
+            <p>{t.text}</p>
+            <button>save</button>
+            <button>edite</button>
+            <button>delete</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
